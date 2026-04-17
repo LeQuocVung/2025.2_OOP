800d3238c45bc2ba133ce61959bb4ee1e2930515
LeQuocVung <shiho310626@gmail.com>
2026-04-17 16:17:10 +0700

    Add class and instance members for DVD id management

--- Lab_02/AimsProject/src/DigitalVideoDisc.java
+++ Lab_02/AimsProject/src/DigitalVideoDisc.java
@@ -1,9 +1,11 @@
 public class DigitalVideoDisc {
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+    private static int nbDigitalVideoDiscs = 0;
     
     public String getTitle() {
         return title;
@@ -22,17 +24,23 @@ public class DigitalVideoDisc {
     }
     public DigitalVideoDisc(String title) {
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -40,9 +48,15 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public int getId() {
+        return id;
     }
 }
