dec1e8ab41383b9a022e5dd0ec28b8ceaac0f197
LeQuocVung <shiho310626@gmail.com>
2026-04-17 22:52:49 +0700

    parameter rework

--- AimsProject/src/Aims.java
+++ AimsProject/src/Aims.java
@@ -2,7 +2,5 @@ public class Aims {
     public static void main(String[] args) {
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Cinderella");
-        System.out.println(dvd1.getId()); 
-        System.out.println(dvd2.getId()); 
     }
 }
